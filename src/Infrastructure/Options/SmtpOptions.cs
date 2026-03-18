79c69005862264b48efdab6f9d044538ff5d2cca
да <denismelnikoww@gmail.com>
2026-03-19 02:55:14 +0300

    чето написал в маркедауне

--- src/Infrastructure/Options/SmtpOptions.cs
+++ src/Infrastructure/Options/SmtpOptions.cs
@@ -8,7 +8,6 @@
         public string Email { get; set; }
         public string Password { get; set; }
 
-        public int MaxConcurrentConnections { get; set; }
         public int MaxRetryAttempts { get; set; } = 3;
         public int TimeoutSeconds { get; set; } = 60;
         public int RetryDelaySeconds { get; set; } = 5;
